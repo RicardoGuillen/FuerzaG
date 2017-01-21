dd2b6f1da08ffb83b68e71bc5dfdfcc33722ca8f
Ricardo Guillen <ricardoguillen@protonmail.com>
2017-01-21 03:22:20 -0600

    Codigo Base de Intellij
    Incluye el .gitignore y una estructura basica para intellij, para ser reconocido señalar en el IDE cual es el directorio src.

---
+++ src/clases/Principal.java
@@ -0,0 +1,13 @@
+package clases;
+
+
+import formularios.frmPrincipal;
+
+public class Principal {
+    public static void main(String args[]){
+
+        frmPrincipal miFormulario = new frmPrincipal();
+        miFormulario.setVisible(true);
+
+    }
+}
---
+++ src/formularios/frmPrincipal.java
@@ -0,0 +1,15 @@
+package formularios;
+
+import javax.swing.*;
+
+/**
+ * Created by administrador on 23/12/16.
+ */
+public class frmPrincipal extends JFrame {
+
+    public frmPrincipal(){
+        setTitle("FuerzaG");
+        setSize(400, 600);
+
+    }
+}
